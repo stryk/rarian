1c08f29d227237dd7bda070138f9234af66cbf92
stryk <dyk418@yahoo.com>
2013-07-16 10:37:24 -0400

    initial barebones commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rarian::Application.config.secret_key_base = 'be2324db6ca97807ecb86e861ab1ac610a4bc247b810126f1f130b437d226557674451053fc7e360dcf191c27a4511c5e021b43651ff83a66b8d6fc6f7881978'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rarian::Application.config.session_store :cookie_store, key: '_rarian_session'
