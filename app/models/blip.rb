ab0b83138267cec2f214075413a89c349dc5f509
Mitesh Jain <mitijain123@gmail.com>
2013-07-18 21:28:33 +0530

    added some validations to the blip

--- app/models/blip.rb
+++ app/models/blip.rb
@@ -2,5 +2,7 @@ class Blip < ActiveRecord::Base
   belongs_to :user
   belongs_to :company
 
+  validates :action, :inclusion => { :in => ["buy", "sell"] }
+
   default_scope { order("created_at desc") }
 end
--- spec/models/blip_spec.rb
+++ spec/models/blip_spec.rb
@@ -3,7 +3,13 @@ require "spec_helper"
 describe Blip do
   it "order by created_at DESC so i should see sell in the first" do
     @blip = FactoryGirl.create(:blip)
-    @blip = FactoryGirl.create(:blip, :user => FactoryGirl.create(:user, :email => "mitesh@mail.com"), :action =>  "sell")
+    @blip = FactoryGirl.create(:blip, :user => FactoryGirl.create(:user, :email => "mitesh@mail.com"), :action => "sell")
     Blip.all.first.action.should eq("sell")
+  end
+
+  it "blip should not be created if action is not buy or sell" do
+    @blip = FactoryGirl.build(:blip, :action => "dont_buy")
+    @blip.save
+    @blip.errors.full_messages.should_not eq([])
   end
 end
