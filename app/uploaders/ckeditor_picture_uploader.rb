8c35106d027e43a82fdb37d07822a36f60ceb07f
Mitesh Jain <mitijain123@gmail.com>
2013-09-24 19:09:55 +0530

    added the  ckeditor fix

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -36,7 +36,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   end
 
   version :content do
-    process :resize_to_limit => [200, 200]
+    process :resize_to_limit => [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
