a0676cf0abf6aeb013802401170d10115cf7dabc
Mitesh Jain <mitijain123@gmail.com>
2013-09-30 16:30:16 +0530

    added the horizontal voting fix

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -12,10 +12,12 @@ class AnswersController < ApplicationController
                              :company_id => @company.id,
                              :content => params[:answer][:content])
     if @answer.errors.blank?
-      flash[:notice] = "Successfully added the answer"
+      #flash[:notice] = "Successfully added the answer"
+      @msg = "Successfully added the answer"
       #redirect_to company_path(@company)
     else
-      flash[:error] = @answer.errors.full_messages.join(",")
+      #flash[:error] = @answer.errors.full_messages.join(",")
+      @msg = @answer.errors.full_messages.join(",")
       render :new, :company_id => params[:company_id], :question_id => params[:question_id]
     end
   end
